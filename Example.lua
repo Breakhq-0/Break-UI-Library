50b2ffc010a600e40bdecefd097e707d714bb032
Breakhq-0 <ce1582032@gmail.com>
2024-11-10 16:53:22 +0000

    Create Example.lua

---
+++ Example.lua
@@ -0,0 +1,67 @@
+local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Breakhq-0/Break-UI-Library/refs/heads/main/Break%20UI%20Library.lua"))()
+
+local Window = library:Create({
+   Name = "Break UI Library" ,
+   Key = Enum.KeyCode.RightShift --open/close key 
+})
+
+local Tab = Window:CreateTab({
+   Name = "Aiming",  
+})
+
+local Tab2 = Window:CreateTab({
+    Name = "Visuals",
+})
+
+
+local Section = Tab:CreateSection({})
+
+local Section2 = Tab2:CreateSection({})
+
+local TextBox = Section:TextBox({
+   callback = function(text)
+       print(text)
+   end,
+})
+
+local Slider = Section:Slider({
+   min = 16, 
+   max = 100,
+   default = 16,
+   callback = function(numbervalue)
+      game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = numbervalue
+   end,
+})
+
+local Toggle = Section:Toggle({
+    Name = "Toggle",
+    callback = function(boolvalue)
+        print(boolvalue)
+    end,
+})
+
+local Button = Section:Button({
+    Name = "Button",
+    callback = function()
+       print('clicked')
+    end,
+})
+
+Button:SetCallback(function()
+    print('changed clicked')
+end)
+
+Button:SetText('New Text')
+
+
+TextBox:SetCallback(function()
+	local text = TextBox:GetText()
+    print("new function"..text)
+end)
+
+
+Toggle:Toggle(true)
+
+local SliderValue = Slider:GetValue()
+
+Slider:SetValue(SliderValue)
