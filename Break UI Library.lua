dd3c425bed8b34a37ee5fe1f4650b1fb125b7251
Breakhq-0 <ce1582032@gmail.com>
2024-12-30 15:24:43 +0000

    Update Break UI Library.lua

--- Break UI Library.lua
+++ Break UI Library.lua
@@ -532,7 +532,7 @@ function UI:Create(Info)
 						local percentage = math.clamp((mouse.X - Slider.SliderBack.AbsolutePosition.X)/(Slider.SliderBack.AbsoluteSize.X),0,1 )
 
 						local sliderValue = options.min + (options.max - options.min)*  percentage
-						local decimal = roundToDecimal(sliderValue,1)
+						local decimal = roundToDecimal(sliderValue,2)
 						Slider.ValueText.Text = tostring(decimal)
 						Slider.SliderMove.Size = UDim2.fromScale(percentage, 1)
 					else
